2fa24154ceedba87963d964b7657fe18ea99fd53
shivam97 <shivam97.dawar@gmail.com>
2020-03-06 16:02:20 +0530

    first commit

--- config/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    serviceId:"",
-    accountSID:"AC0436ab9a97d0b1ea262e6d855b8bf614",
-    authToken:"7d82ace7ddac38ccd21d46080a33f50b",
-    twilioPhoneNumber:"+14703308801"
-}
--- src/index.js
+++
@@ -1,6 +0,0 @@
-const app = require('./app')
-const port = process.env.PORT 
-
-app.listen(port,_=>{
-    console.log('Server is up on port '+ port)
-})
