87fe7507fd5bf8ba6518c108ca3f6efc45dfbfa3
shivam97 <shivam97.dawar@gmail.com>
2020-03-18 18:34:24 +0530

    deployment ready

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    serviceId:"VA78ddf46317e2eed6be863e6c9a7d68b4",
-    accountSID:"AC0436ab9a97d0b1ea262e6d855b8bf614",
-    authToken:"7d82ace7ddac38ccd21d46080a33f50b",
-    twilioPhoneNumber:"+14703308801"
+    serviceId:process.env.SERVICE_ID,
+    accountSID:process.env.ACCOUNT_SID,
+    authToken:process.env.AUTH_TOKEN,
+    twilioPhoneNumber:process.env.TWILLIO_NUMBER
 }
