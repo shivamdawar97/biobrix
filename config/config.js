ddd61508daa4ce3eb716f980378fc3d3eb4870f7
shivam97 <shivam97.dawar@gmail.com>
2020-05-15 03:07:21 +0530

    firebase-admin-2

--- config/config.js
+++ config/config.js
@@ -1,16 +1,3 @@
-process.env.GOOGLE_APPLICATION_CREDENTIALS = {
-    type: "service_account",
-    project_id: "ng-complete1",
-    private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
-    private_key: process.env.GOOGLE_PRIVATE_KEY,
-    client_email: "firebase-adminsdk-ygnkx@ng-complete1.iam.gserviceaccount.com",
-    client_id: process.env.GOOGLE_CLIENT_ID,
-    auth_uri: "https://accounts.google.com/o/oauth2/auth",
-    token_uri: "https://oauth2.googleapis.com/token",
-    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-    client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-ygnkx%40ng-complete1.iam.gserviceaccount.com"
-}
-
 module.exports = {
     serviceId:process.env.TWILIO_SERVICE_ID,
     accountSID:process.env.TWILIO_ACCOUNT_SID,
