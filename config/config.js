7add5fc5af0c00ae23f94646b25f6b6b4a14e46e
Shivam Dawar <36306151+shivamdawar97@users.noreply.github.com>
2020-05-15 18:56:37 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 const paytmConfig = {
-    mid : process.env.PAYTM_MERCH_ID || 'GpfBCX42323873899427',
-    key : process.env.PAYTM_MERCH_KEY || 'HFfseLgiqJf7ZLvB',
+    mid : process.env.PAYTM_MERCH_ID ,
+    key : process.env.PAYTM_MERCH_KEY ,
     website: process.env.PAYTM_MERCH_WEBSITE || 'WEBSTAGING',
     transactionUrl: process.env.PAYTM_TXN_URL || 'https://securegw-stage.paytm.in/theia/processTransaction',
     hostName:  process.env.PAYTM_HOST || 'securegw-stage.paytm.in'
