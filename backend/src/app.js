0596dd7cb810fbb6bd5c73344722a907153a8fe7
shivam97 <shivam97.dawar@gmail.com>
2020-06-04 09:34:07 +0530

    sep-1

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,3 @@
-const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
@@ -12,8 +11,6 @@ const uploadRouter = require('./routers/file_upload')
 const reviewRouter = require('./routers/review_router')
 const homepageRouter = require('./routers/homepage_router')
 const paytmRoutes = require('./paytm/controller').router
-const angularPath = path.join(__dirname,'../build')
-
 
 
 app.use(function(req, res, next) {
@@ -30,7 +27,6 @@ app.use(bodyParser.urlencoded({extended: false}))
 // app.use(express.json())
 
 app.use('/images/product-images',express.static('images/product-images'))
-app.use(express.static(angularPath))
 app.use(productRouter)
 app.use(orderRouter)
 app.use(authRouter)
@@ -39,6 +35,12 @@ app.use(uploadRouter)
 app.use(reviewRouter)
 app.use(homepageRouter)
 app.use('/paytm',paytmRoutes)
+
+/*
+const path = require('path')
+const angularPath = path.join(__dirname,'../build')
+app.use(express.static(angularPath))
 app.use('*',express.static(angularPath))
+*/
 
 module.exports = app
