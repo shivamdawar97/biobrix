4224bf375d72bd0d6baadb024f0a8c6fb959494d
shivam97 <shivam97.dawar@gmail.com>
2020-07-06 17:53:19 +0530

    chat1

--- frontend/bioc/src/environments/environment.ts
+++ frontend/bioc/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  BASE_URL: 'https://biobrix-healthcare.herokuapp.com/'
-  // BASE_URL: 'http://localhost:3000/'
+  //BASE_URL: 'https://biobrix-healthcare.herokuapp.com/'
+   BASE_URL: 'http://localhost:3000/'
   // BASE_URL: 'http://192.168.100.5:3000/'
 };
 
