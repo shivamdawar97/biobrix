fb018e0e04e5aaeade62d945579f361602f05926
shivam97 <shivam97.dawar@gmail.com>
2021-02-06 18:44:02 +0530

    restart-bottom

--- frontend/bioc/src/app/components/footer/footer.component.ts
+++ frontend/bioc/src/app/components/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { ContactUsService } from 'src/app/core/http/contact-us.service';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +9,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  name = new FormControl('', [Validators.required]);
+  email = new FormControl('', [Validators.required,Validators.email]);
+  message = new FormControl('', [Validators.required]);
+
+  isLoading = false;
+
+  constructor(private contactUsService: ContactUsService) { }
 
   ngOnInit(): void {
   }
+
+  submitForm() {
+    if (this.name.valid && this.email.valid && this.message.valid) {
+      this.sendMessage(this.name.value,this.email.value,this.message.value);
+      this.name.reset(); this.email.reset(); this.message.reset();
+    }
+  }
+
+  private sendMessage(name: string,contact: string, message: string) {
+    this.isLoading = true;
+    this.contactUsService.sendMessage({name,contact,message}).subscribe( send => {
+      this.isLoading = false;
+      this.showMessage('Your message has been sent successfully.')
+      console.log(send)
+    }, err => {
+      this.isLoading = false;
+      this.showMessage('Error in sending your message.',true);
+    }
+    )
+  }
+
+  private showMessage(message: string, error: boolean = false) {
+
+    const msg = document.createElement('div');
+    msg.innerText = message;
+    msg.classList.add('alert');
+    msg.classList.add(error?'alert-danger':'alert-success');
+    msg.classList.add('alert-msg');
+    msg.classList.add('animated');
+    msg.classList.add('slideInUp');
+    document.body.appendChild(msg);
+
+    setTimeout(() => {
+      document.body.removeChild(msg);
+    }, 4000);
+  }
+
+
 
 }
--- frontend/bioc/src/app/modules/home/contact/contact.component.ts
+++ frontend/bioc/src/app/modules/home/contact/contact.component.ts
@@ -20,41 +20,5 @@ export class ContactComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  submitForm() {
-    if (this.name.valid && this.email.valid && this.message.valid) {
-      this.sendMessage(this.name.value,this.email.value,this.message.value);
-      this.name.reset(); this.email.reset(); this.message.reset();
-    }
-  }
-
-  private sendMessage(name: string,contact: string, message: string) {
-    this.isLoading = true;
-    this.contactUsService.sendMessage({name,contact,message}).subscribe( send => {
-      this.isLoading = false;
-      this.showMessage('Your message has been sent successfully.')
-      console.log(send)
-    }, err => {
-      this.isLoading = false;
-      this.showMessage('Error in sending your message.',true);
-    }
-    )
-  }
-
-  private showMessage(message: string, error: boolean = false) {
-
-    const msg = document.createElement('div');
-    msg.innerText = message;
-    msg.classList.add('alert');
-    msg.classList.add(error?'alert-danger':'alert-success');
-    msg.classList.add('alert-msg');
-    msg.classList.add('animated');
-    msg.classList.add('slideInUp');
-    document.body.appendChild(msg);
-
-    setTimeout(() => {
-      document.body.removeChild(msg);
-    }, 4000);
-  }
-
 
 }
